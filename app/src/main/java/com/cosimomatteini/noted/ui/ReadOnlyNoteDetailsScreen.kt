a97dd5dce1bf2a8f32a245029a2e35ada86321e9
Cosimo Matteini <dev.matteini@gmail.com>
2026-06-15 22:46:34 +0200

    note: make archived and discarded notes selectable

--- app/src/main/java/com/cosimomatteini/noted/ui/ReadOnlyNoteDetailsScreen.kt
+++ app/src/main/java/com/cosimomatteini/noted/ui/ReadOnlyNoteDetailsScreen.kt
@@ -4,6 +4,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,20 +21,24 @@ internal fun ReadOnlyNoteDetailsScreen(
     NoteDetailsScaffold(onBack = onBack) { innerPadding ->
         NoteDetailsContentColumn(innerPadding = innerPadding) {
             if (title.isNotEmpty()) {
-                Text(
-                    text = title,
-                    style = noteTitleTextStyle(),
-                    modifier = Modifier.fillMaxWidth()
-                )
+                SelectionContainer {
+                    Text(
+                        text = title,
+                        style = noteTitleTextStyle(),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             }
             if (description.isNotEmpty()) {
-                Text(
-                    text = savedNoteDescriptionAnnotatedString(description),
-                    style = noteDescriptionTextStyle(),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                )
+                SelectionContainer(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = savedNoteDescriptionAnnotatedString(description),
+                        style = noteDescriptionTextStyle(),
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
             } else {
                 Spacer(Modifier.weight(1f))
             }
