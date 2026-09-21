97e1086e6041b2cd2005df31594e9b5c77a26ac2
Cosimo Matteini <dev.matteini@gmail.com>
2026-09-21 23:15:42 +0200

    release 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.cosimomatteini.noted"
         minSdk = 35
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
